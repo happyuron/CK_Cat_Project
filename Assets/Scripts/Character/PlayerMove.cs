9903ed6dfc2eb49fc95dc54a424f7c6c5e12004c
happyuron <87974802+happyuron@users.noreply.github.com>
2022-05-05 12:16:18 +0900

    Add Packages and Scripts

---
+++ Assets/Scripts/Character/PlayerMove.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+public class PlayerMove : Character
+{
+    private Rigidbody2D rigid;
+    public void Move(InputAction.CallbackContext ctx)
+    {
+
+    }
+}
