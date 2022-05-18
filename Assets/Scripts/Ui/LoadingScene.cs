a67985d8a7f89cc370eb255d2631e89e1c6b8878
happyuron <87974802+happyuron@users.noreply.github.com>
2022-05-19 02:35:24 +0900

    Update

--- Assets/Scripts/Ui/LoadingScene.cs
+++ Assets/Scripts/Ui/LoadingScene.cs
@@ -10,6 +10,7 @@ public class LoadingScene : MonoBehaviour
 {
     [SerializeField] private Image background;
 
+    [SerializeField] private TextMeshProUGUI text;
 
     private RectTransform rectTr;
 
@@ -36,14 +37,14 @@ public class LoadingScene : MonoBehaviour
         float setX = value == 1 ? -1920 : 1920;
         float dirX = value == 1 ? 1920 : -1920;
         rectTr.localPosition = new Vector3(setX, 0, 0);
-        Vector2 dirVec = new Vector2(dirX, 0);
+        Vector2 dirVec = Vector2.zero;
 
 
         while (true)
         {
             yield return null;
             rectTr.localPosition = Vector2.MoveTowards(rectTr.localPosition, dirVec, 50);
-            if (Vector2.Distance(rectTr.localPosition, new Vector2(0, 0)) <= 60)
+            if (Vector2.Distance(rectTr.localPosition, new Vector2(0, 0)) <= 1)
             {
                 AsyncOperation op = SceneManager.LoadSceneAsync(index);
                 op.allowSceneActivation = false;
@@ -55,12 +56,23 @@ public class LoadingScene : MonoBehaviour
                         op.allowSceneActivation = true;
                     }
                 }
+                text.gameObject.SetActive(true);
+                while (true)
+                {
+                    yield return null;
+                    if (Input.GetMouseButtonDown(0))
+                    {
+                        dirVec = new Vector2(dirX, 0);
+                        text.gameObject.SetActive(false);
+                        break;
+                    }
+                }
 
             }
             if (Vector2.Distance(rectTr.localPosition, dirVec) < 10)
             {
                 gameObject.SetActive(false);
-                //UiManager.Instance.HideSetMenu();
+                UiManager.Instance.HideSetMenu();
                 break;
             }
         }
